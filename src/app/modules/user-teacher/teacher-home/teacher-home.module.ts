49275211b66209f751a01a904cc13bd25c54fa51
spavel-gh <spavel.mail@gmail.com>
2021-03-07 21:28:37 +0200

    add group list component whith mock-data

--- src/app/modules/user-teacher/teacher-home/teacher-home.module.ts
+++ src/app/modules/user-teacher/teacher-home/teacher-home.module.ts
@@ -3,14 +3,21 @@ import { RouterModule } from '@angular/router';
 import { teacherHomeRouting } from './teacher-home.routing';
 import { TeacherHomeViewComponent } from './components';
 import { SharedHeaderModule } from '../../shared';
+import { GroupListComponent } from './components';
+
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   imports: [
     RouterModule.forChild(teacherHomeRouting),
+    MatTableModule,
     SharedHeaderModule
   ],
   exports: [TeacherHomeViewComponent],
-  declarations: [TeacherHomeViewComponent],
+  declarations: [
+    TeacherHomeViewComponent, 
+    GroupListComponent,
+  ],
   providers: [],
 })
 export class TeacherHomeModule { }
