de3590e1bcc73a9850ccbc1f6cdc7f60fa05f630
spavel-gh <spavel.mail@gmail.com>
2021-04-13 20:02:24 +0300

    delete imports and style fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxsModule } from '@ngxs/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedBreadcrumbsModule } from './modules/shared/shared-breadcrumbs';
 import { SharedHeaderModule } from './modules/shared/shared-header';
 import { AuthGuard } from './modules/auth/guards';
 import { AuthService } from './modules/auth/auth-page/services';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LayoutComponent } from './modules/shared/layout/layout.component';
 import { TokenInterceptor } from './modules/auth/auth-page/interceptors/token.interceptor';
-import { environment } from 'src/environments/environment';
 import { UserState } from './modules/shared/user-store/user-state';
 import { UserService } from './modules/shared/user-store/services/user.service';
 import { MapResponseService } from './modules/shared/helper/services/map-response.service';
-import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
 @NgModule({
   declarations: [
