0207f97a864e5081ec072ff44b03ca539da2d99b
spavel-gh <spavel.mail@gmail.com>
2021-04-18 20:22:32 +0300

    update sort section

--- src/app/modules/user-teacher/teacher-tasks/components/tasks-for-class/tasks-for-class.component.ts
+++ src/app/modules/user-teacher/teacher-tasks/components/tasks-for-class/tasks-for-class.component.ts
@@ -5,6 +5,12 @@ import { Post } from 'src/models';
 import { TeacherGetPosts } from '../../../teacher-post/store/actions';
 import { TeacherPostState } from '../../../teacher-post/store/teacher-post.state';
 
+export interface SortOption {
+  name: string;
+  selected: boolean;
+  disabled: boolean;
+}
+
 @Component({
   selector: 'app-tasks-for-class',
   templateUrl: './tasks-for-class.component.html',
@@ -13,6 +19,24 @@ import { TeacherPostState } from '../../../teacher-post/store/teacher-post.state
 export class TasksForClassComponent implements OnInit {
 
   @Select(TeacherPostState.getPosts) posts$!: Observable<Post[]>;
+
+  sortOptions: SortOption[] = [
+    {
+      name: 'всі',
+      selected: true,
+      disabled: false
+    },
+    {
+      name: 'завдання',
+      selected: false,
+      disabled: true
+    },
+    {
+      name: 'повідомлення',
+      selected: false,
+      disabled: true
+    },
+  ];
 
   constructor( private store: Store) { }
 
