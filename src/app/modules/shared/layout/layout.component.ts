222e4a02d501f33eaa2d3303cde7dfca78225dde
spavel-gh <spavel.mail@gmail.com>
2021-05-09 16:25:45 +0300

    add ambient background on layout component

--- src/app/modules/shared/layout/layout.component.ts
+++ src/app/modules/shared/layout/layout.component.ts
@@ -7,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
+  cats = 6;
+  cat = 0;
+  houseplants = 3;
+  houseplant = 0;
+
   constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.cat = this.getRandomInt(this.cats);
+    this.houseplant = this.getRandomInt(this.houseplants);
+  }
 
+  getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
+  }
 }
