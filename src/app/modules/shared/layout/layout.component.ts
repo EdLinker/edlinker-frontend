be0e78c3070aef73b6c106b44a9b8bb274aea7f2
Ronya <Ronyahavuka@gmail.com>
2021-05-11 16:34:17 +0300

    changed

--- src/app/modules/shared/layout/layout.component.ts
+++ src/app/modules/shared/layout/layout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -8,19 +8,28 @@ import { Router } from '@angular/router';
 })
 export class LayoutComponent implements OnInit {
 
+  hide!: boolean;
   cats = 6;
   cat = 0;
   houseplants = 3;
   houseplant = 0;
 
-  constructor( public router: Router ) { }
+  constructor( public router: Router) {}
 
   ngOnInit(): void {
     this.cat = this.getRandomInt(this.cats);
     this.houseplant = this.getRandomInt(this.houseplants);
+    this.hideBreadCrumb();
   }
 
   getRandomInt(max: number) {
     return Math.floor(Math.random() * max);
+  }
+
+  hideBreadCrumb() {
+    if (this.router.url === 'student') {
+      return  this.hide = true;
+    }
+    return this.hide = false;
   }
 }
